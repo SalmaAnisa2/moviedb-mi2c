9b7e029122540ae816765d32b16da1cd9de992c7
SalmaAnisa2 <anisasyukrisalma@gmail.com>
2025-05-19 14:49:39 +0700

    Seeder dan Category

--- database/seeders/CategorySeeder.php
+++ database/seeders/CategorySeeder.php
@@ -10,40 +10,42 @@ class CategorySeeder extends Seeder
 {
     /**
      * Run the database seeds.
+     *
+     * @return void
      */
-    public function run(): void
-    {
-        DB::table('categories')->insert([
-           [
-            'category_name'=> 'Action',
-            'description'=> 'Film dengan adegan-adengan penuh aksi dan ketegangan.',
+    public function run(): void 
+{
+    DB::table('categories')->insert([
+        [
+            'category_name' => 'Action',
+            'description' => 'Film dengan adegan-adegan dengan penuh aksi dan ketegangan.',
             'created_at' => now(),
-            'updated_at' => now(),
-           ],
-           [
-             'category_name'=> 'Comdey',
-            'description'=> 'Film yang bertujuan untuk menghibur dan mengundang tawa.',
+            'updated_at' => now(),    
+        ],
+        [
+            'category_name' => 'Comedy',
+            'description' => 'Film yang bertujuan untuk menghibur dan mengundang tawa.',
             'created_at' => now(),
-            'updated_at' => now(),
-           ],
-           [
-             'category_name'=> 'Drama',
-            'description'=> 'Film dengan adegan-adengan penuh aksi dan ketegangan.',
+            'updated_at' => now(), 
+        ],
+        [
+            'category_name' => 'Drama',
+            'description' => 'Film yang berfokus pada pengembangan karakter dan konflik emosional.',
             'created_at' => now(),
-            'updated_at' => now(),
-           ],
-           [
-             'category_name'=> 'Sci-Fi',
-            'description'=> 'Film dengan adegan-adengan penuh aksi dan ketegangan.',
+            'updated_at' => now(), 
+        ],
+        [
+            'category_name' => 'Sci-Fi',
+            'description' => 'Film dengan latar belakang ilmiah dan teknologi futuristik.',
             'created_at' => now(),
-            'updated_at' => now(),
-           ],
-           [
-             'category_name'=> 'Romance',
-            'description'=> 'Film dengan adegan-adengan penuh aksi dan ketegangan.',
+            'updated_at' => now(), 
+        ],
+        [
+            'category_name' => 'Romance',
+            'description' => 'Film yang berpusat pada kisah cinta dan hubungan romantis.',
             'created_at' => now(),
-            'updated_at' => now(),
-           ],
-        ]);
-    }
+            'updated_at' => now(), 
+        ],
+    ]);
+}
 }
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,24 +2,25 @@
 
 namespace Database\Seeders;
 
-use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Movie;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class DatabaseSeeder extends Seeder
 {
     /**
-     * Seed the application's database.
+     * Run the database seeds.
      */
+    
     public function run(): void
     {
         // User::factory(10)->create();
 
-        //User::factory()->create([
-            //'name' => 'Test User',
-            //'email' => 'test@example.com',
-        //]);
-        
+        // User::factory()->create([
+        //     'name' => 'Test User',
+        //     'email' => 'test@example.com',
+        // ]);
         $this->call(CategorySeeder::class);
         Movie::factory(50)->create();
     }
